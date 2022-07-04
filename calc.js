eb8b73271e7f2d978ee114c07a156066c3030da8
surya-357 <101453261+surya-357@users.noreply.github.com>
2022-07-04 17:11:29 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,24 +1,19 @@
-// let string="surya";
-// let string1=string.replace("s","b");
-// let string2=string1.replace("u","e");
-// let string3=string2.replace("a","st");
-// console.log(string3)
+var val=document.getElementById("surya");
+        
+function add(v){
+val.value+=v;
+}
 
 
+function cls(){
+val.value="";
+}
 
-const today=new Date();
-const day=today.getDay();
-const date=today.getDate();
-const daylist=['sunday','Monday','tuesday','wednesday','thursday','friday','saturday']
-const month=today.getMonth()+1;
-const year=today.getFullYear();
-console.log('Today is:'+date+"/"+month+"/"+year+"/"+daylist[day])
-
-let hour=today.getHours();
-let miniute=today.getMinutes();
-let second=today.getSeconds();
-let prepand=(hour>=12)?'pm':'am';
-hour=(hour>=12)?hour-12:hour;
+function exe(){
+val.value=eval(val.value);
+}
 
 
-console.log('Today Time Is:'+hour+':'+miniute+':'+second+':'+prepand)
+function cancel(){
+val.value=val.value.slice(0,val.value.length-1);
+}
